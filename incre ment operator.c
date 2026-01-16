76a195336643c02624c3081f455659a06f5d3e60
unknown <yadavdn72@gmail.com>
2026-01-16 00:17:02 -0800

    all

---
+++ incre ment operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+	int a=10,x=11,m=20,n,y;
+	a++;
+	printf( "\na=%d",a);
+	a--;
+	printf("\n a=%d",a);
+	y=x++;
+	printf("\n y=%d x=%d",y,x);
+	n=++m;
+	printf("\n n=%d,m=%d",n,m);
+	
+	
+}
